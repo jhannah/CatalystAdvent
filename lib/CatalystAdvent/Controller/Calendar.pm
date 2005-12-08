26de9c26590f6243f94509cc0559094f013349dc
Andy Grundman <andy@hybridized.org>
2005-12-08 19:18:06 +0000

    Calendar, fixed stat bug in rss feed, perltidy

--- lib/CatalystAdvent/Controller/Calendar.pm
+++ lib/CatalystAdvent/Controller/Calendar.pm
@@ -33,7 +33,7 @@ Detaches to the "year" display for the current year.
 =cut
 
 sub index : Private {
-    my( $self, $c ) = @_;
+    my ( $self, $c ) = @_;
     $c->detach( 'year', [ $c->stash->{now}->year ] );
 }
 
@@ -44,13 +44,13 @@ Displays the calendar for any given year
 =cut
 
 sub year : Regex('^(\d{4})$') {
-    my( $self, $c, $year ) = @_;
-    $year ||= $c->req->snippets->[ 0 ];
-    $c->res->redirect( $c->uri_for( '/' ) )
+    my ( $self, $c, $year ) = @_;
+    $year ||= $c->req->snippets->[0];
+    $c->res->redirect( $c->uri_for('/') )
         unless ( -e $c->path_to( 'root', $year ) );
-    $c->stash->{ year }     = $year;
-    $c->stash->{ calendar } = calendar( 12, $year );
-    $c->stash->{ template } = 'year.tt';
+    $c->stash->{year}     = $year;
+    $c->stash->{calendar} = calendar( 12, $year );
+    $c->stash->{template} = 'year.tt';
 }
 
 =head2 day
@@ -61,32 +61,32 @@ pod to html.
 =cut
 
 sub day : Regex('^(\d{4})/(\d\d?)$') {
-    my( $self, $c, $year, $day ) = @_;
-    $year ||= $c->req->snippets->[ 0 ];
-    $day  ||= $c->req->snippets->[ 1 ];
+    my ( $self, $c, $year, $day ) = @_;
+    $year ||= $c->req->snippets->[0];
+    $day  ||= $c->req->snippets->[1];
 
-    $c->detach('year',[$year]) 
-        unless( -e ( my $file = $c->path_to( 'root', $year, "$day.pod" ) ) ); 
-    $c->stash->{ calendar } = calendar( 12, $year );
-    $c->stash->{ year }     = $year;
-    $c->stash->{ day }      = $day;
-    $c->stash->{ template } = 'day.tt';
-    
+    $c->detach( 'year', [$year] )
+        unless ( -e ( my $file = $c->path_to( 'root', $year, "$day.pod" ) ) );
+    $c->stash->{calendar} = calendar( 12, $year );
+    $c->stash->{year}     = $year;
+    $c->stash->{day}      = $day;
+    $c->stash->{template} = 'day.tt';
+
     # cache the generated XHTML file so we're not parsing it on every request
-    my $mtime = (stat $file)->mtime;
-    my $cached_pod = $c->cache->get( "$file $mtime" );
+    my $mtime      = ( stat $file )->mtime;
+    my $cached_pod = $c->cache->get("$file $mtime");
     if ( !$cached_pod ) {
-        my $parser = Pod::Xhtml->new( 
-            StringMode   => 1, 
-            FragmentOnly => 1, 
-            MakeIndex    => 0, 
+        my $parser = Pod::Xhtml->new(
+            StringMode   => 1,
+            FragmentOnly => 1,
+            MakeIndex    => 0,
             TopLinks     => 0
         );
-        $parser->parse_from_file( "$file" );
+        $parser->parse_from_file("$file");
         $cached_pod = $parser->asString;
         $c->cache->set( "$file $mtime", $cached_pod, '12h' );
     }
-    $c->stash->{ pod } = $cached_pod;
+    $c->stash->{pod} = $cached_pod;
 }
 
 =head2 rss
@@ -96,35 +96,38 @@ Generates an rss feed of tips for the given year.
 =cut
 
 sub rss : Global {
-    my( $self, $c, $year ) = @_;
-    my $feed=XML::Feed->new('RSS');
+    my ( $self, $c, $year ) = @_;
+    my $feed = XML::Feed->new('RSS');
     $year ||= $c->stash->{now}->year;
-    $feed->title($c->config->{name}. ' RSS Feed');
-    $feed->link($c->req->base);
+    $feed->title( $c->config->{name} . ' RSS Feed' );
+    $feed->link( $c->req->base );
     $feed->description('Catalyst advent calendar');
-    $year ||= $c->req->snippets->[ 0 ];
-    $c->res->redirect( $c->uri_for( '/' ) )
+    $year ||= $c->req->snippets->[0];
+    $c->res->redirect( $c->uri_for('/') )
         unless ( -e $c->path_to( 'root', $year ) );
-    $c->stash->{ year }     = $year;
-    my ($day,$entries)      = (24, 0);
-    my $feed_mtime=0;
-    while ($day>0 && $entries<5) {
-        if ( -e ( my $file = $c->path_to( 'root', $year, "$day.pod" ) ) )  {
-            my ($mtime,$ctime) = (stat($file))[9,10];
-            $feed_mtime=$mtime if $mtime>$feed_mtime;
+    $c->stash->{year} = $year;
+    my ( $day, $entries ) = ( 24, 0 );
+    my $feed_mtime = 0;
+
+    while ( $day > 0 && $entries < 5 ) {
+        if ( -e ( my $file = $c->path_to( 'root', $year, "$day.pod" ) ) ) {
+            my $stat = stat $file;
+            my $mtime = $stat->mtime;
+            my $ctime = $stat->ctime;
+            $feed_mtime = $mtime if $mtime > $feed_mtime;
             my $entry = XML::Feed::Entry->new('RSS');
             $entry->title("Calendar entry for day $day.");
-            $entry->link($c->uri_for("/$year/$day"));
-            $entry->issued(DateTime->from_epoch(epoch=>$ctime));
-            $entry->modified(DateTime->from_epoch(epoch=>$mtime));
+            $entry->link( $c->uri_for("/$year/$day") );
+            $entry->issued( DateTime->from_epoch( epoch   => $ctime ) );
+            $entry->modified( DateTime->from_epoch( epoch => $mtime ) );
 
             $feed->add_entry($entry);
-            $entries++;   
+            $entries++;
         }
         $day--;
     }
-    $feed->modified(DateTime->from_epoch(epoch=>$feed_mtime));
-    $c->res->body($feed->as_xml);
+    $feed->modified( DateTime->from_epoch( epoch => $feed_mtime ) );
+    $c->res->body( $feed->as_xml );
     $c->res->content_type('application/rss+xml');
 }
 
